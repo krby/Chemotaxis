34cf4c0a06c5175e91bd0668b2140aa436d9e051
kichoy <kichoy@697-334-Sauron.sf.k12.internal>
2014-09-18 11:15:29 -0700

    primitive

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;   
-void setup()   
+public void setup()   
 {     
 	size (400, 400);
 	colony = new Bacteria[100];
@@ -9,7 +25,7 @@ void setup()
 	}
 }
 
-void draw()
+public void draw()
 {    
 	background(100);
 	for (int i = 0; i < colony.length; i++)
@@ -33,7 +49,7 @@ class Bacteria
 		myClr = color (155);
 	}
 
-	void move()
+	public void move()
 	{
 		// myX = myX + (((int)(Math.random()*3))-1);
 		// myY = myY + (((int)(Math.random()*3))-1);
@@ -60,9 +76,18 @@ class Bacteria
 
 	}
 
-	void show()
+	public void show()
 	{
 		fill (myClr);
 		ellipse (myX, myY, 10, 10);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
